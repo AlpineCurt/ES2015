7a88dff2aa6534fca940dbbb5fd6be914a7940a6
Curt <34628026+AlpineCurt@users.noreply.github.com>
2022-06-30 12:25:02 -0700

    Add files via upload

---
+++ let_const.js
@@ -0,0 +1,12 @@
+const PI = 3.14;
+
+/* 
+1.  var is function scoped, while let is block scoped.  var can be redeclared, but let cannot
+
+2.  var is function scoped, while const is block scoped.  const cannot be reassigned or redeclared.
+
+3.  let can be reassigned, but const cannot.
+
+4.  Hoising is where variables declared with var are all declared (but not assigned) prior to any other code being compiled.
+
+*/
